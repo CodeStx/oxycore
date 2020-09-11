90bdcabbc9fbff9d6005260cf4a3949b4fc8733f
Duwles <duwless@outlook.com>
2020-09-11 21:06:06 +0200

    Create Config.php

---
+++ src/oxycore/framework/Config/Config.php
@@ -0,0 +1,24 @@
+<?php
+namespace Oxycore\Framework\Config;
+
+use function Oxycore\Framework\Boost\includeIfExist;
+
+class Config
+{
+    private array $_config;
+
+    public function __construct()
+    {
+        $this->_config = include APP_PATH . "/config/config.php";
+    }
+
+    public function __get($key)
+    {
+        return $this->_config[$key];
+    }
+
+    public function getAll(): array
+    {
+        return $this->_config;
+    }
+}
